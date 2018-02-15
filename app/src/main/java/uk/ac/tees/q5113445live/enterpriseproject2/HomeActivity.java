66aca3ee5f66d69d9bfaee35f827eff165e92f13
Unknown <q5113445@live.tees.ac.uk>
2018-02-15 20:34:50 +0000

    Able to see name of user on homepage.

--- app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/HomeActivity.java
+++ app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/HomeActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,17 +27,17 @@ public class HomeActivity extends AppCompatActivity
         String userId = user.getUid();
 
 
-        mDatabase = FirebaseDatabase.getInstance().getReference("users");
-        mDatabase.orderByValue()
+        mDatabase = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+
         ValueEventListener userListener = new ValueEventListener()
         {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot)
             {
                 User user = dataSnapshot.getValue(User.class);
+                System.out.println(user);
                 TextView userText = findViewById(R.id.showUserEmail);
-
-
+                userText.setText(user.getName());
 
 
                 System.out.println("HELLO");
