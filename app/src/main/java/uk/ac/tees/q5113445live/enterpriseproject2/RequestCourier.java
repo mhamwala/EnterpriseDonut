42a95c44c8e99e7b796510ef36bda26e1fc7ffff
Unknown <q5113445@live.tees.ac.uk>
2018-02-20 12:54:44 +0000

    Try

--- app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/RequestCourier.java
+++ app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/RequestCourier.java
@@ -10,7 +10,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AdvertiseItem extends AppCompatActivity
+public class RequestCourier extends AppCompatActivity
 {
 
     private FirebaseAuth mAuth;
