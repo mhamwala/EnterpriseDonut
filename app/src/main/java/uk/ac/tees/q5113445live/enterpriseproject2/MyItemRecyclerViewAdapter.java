67ae9a5898aeae59bf337dac3c82c9f6f83c2a9c
James Liddle <jimmylid@Jamess-MacBook-Air.local>
2018-04-19 20:34:19 +0100

    updated layout and redsigned some pages
    i have redsigned the card layout in prep for getting the image to show , changed the buttons so that they all match on everypage

--- app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/uk/ac/tees/q5113445live/enterpriseproject2/MyItemRecyclerViewAdapter.java
@@ -7,13 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
 
 import uk.ac.tees.q5113445live.enterpriseproject2.JobFragment.OnListFragmentInteractionListener;
 
@@ -44,6 +48,9 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
     private HashMap<String, String> advertMap;
     private int pos;
     private ArrayList value;
+    private StorageReference mStorageRef;
+    private ImageView imageView;
+    private View view;
 
 
     public MyItemRecyclerViewAdapter(List<Advert> items, OnListFragmentInteractionListener listener)
